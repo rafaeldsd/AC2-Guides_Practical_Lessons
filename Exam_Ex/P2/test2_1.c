f4bc36eb3d39a694b851d270fbb6610dc437fe32
Rafael Dias <rafaeldsd93@gmail.com>
2023-05-25 16:22:59 +0100

    updates in Exam_Ex

--- Exam_Ex/P2/test2_1.c
+++ Exam_Ex/P2/test2_1.c
@@ -1,41 +1,38 @@
 #include <detpic32.h>
 
 int main(void){
-    // Config RB0 and RB3
+    // Config I/O
     TRISB = TRISB | 0x0009;
 
-    // Config Timer 3 (280 Hz)
-    T3CONbits.TCKPS = 1; // 1:2 prescaler 
+    T3CONbits.TCKPS = 1; // 1:2
     PR3 = 35714; // Fout = 20MHz / (2 * (35714 + 1)) = 280 Hz
-    TMR3 = 0; // Clear timer T2 count register
-    T3CONbits.TON = 1; // Enable timer T2 (must be the last command of the timer configuration sequence)
+    TMR3 = 0; // Reset timer T3 count register
+    T3CONbits.TON = 1; // Enable timer T3
 
     OC2CONbits.OCM = 6; // PWM mode on OCx; fault pin disabled
-    OC2CONbits.OCTSEL = 1;// Use timer T3 as the time base for PWM generation
-    OC2CONbits.ON = 1; // Enable OC1 module
+    OC2CONbits.OCTSEL = 1;// Use timer T2 as the time base for PWM generation
+    OC2RS = 12500; // Ton constant
+    OC2CONbits.ON = 1; // Enable OC2 module
+    int value;
 
-    while (1){
+    while(1){
         // 250 us
         resetCoreTimer();
-        while(readCoreTimer<5000);
-
-
-        portVal = PORTB & 0x0009;
-        switch (portValue){
-            case 0x1: 
+        while(readCoreTimer()< 5000); // 1 ms => 20000 | 1 us => 20 | 250 us => 20 * 250 = 5000
+        value = PORTB & 0x0009;
+        if (value == 0x1){
             setPWM(25);
-            break;
-            case 0x8:
-            setPWM(75);
-            break;
+        }
+        if (value == 0x8){
+            setPWM(70);
         }
 
     }
-    return 0;
+
 }
 
-
 void setPWM(int dutycycle){
-    if (dutycycle >= 0 && dutycycle <= 100)
-        OC2RS= ((PR3+1)*dutycycle)/100;
+    if(dutycycle >= 0 && dutycycle <= 100){
+        OC2RS = ((PR3 + 1) * dutycycle)/100;
+    }
 }
--- Exam_Ex/P2/test2_2.c
+++ Exam_Ex/P2/test2_2.c
@@ -1,24 +1,24 @@
 #include <detpic32.h>
 
+volatile int voltage = 0; // Global variable  
+
 int main(void){
-
-    // Config Displays
-    TRISDbits.TRISD6 = 0;
-	TRISDbits.TRISD5 = 0;
+    // Configure I/O
     TRISB = TRISB & 0x80FF;
-    LATB = LATB & 0x80FF;
+    TRISDbits.TRISD5 = 0;
+    TRISDbits.TRISD6 = 0;
 
-    // Config ADC
+    // Configure ADC 
     TRISBbits.TRISB4 = 1; // RB4 digital output disconnected
     AD1PCFGbits.PCFG4= 0; // RB4 configured as analog input
     AD1CON1bits.SSRC = 7; // Conversion trigger selection bits: in this mode an internal counter ends sampling and starts conversion
     AD1CON1bits.CLRASAM = 1; // Stop conversions when the 1st A/D converter interrupt is generated. At the same time, hardware clears the ASAM bit
     AD1CON3bits.SAMC = 16; // Sample time is 16 TAD (TAD = 100 ns)
-    AD1CON2bits.SMPI = 2-1; // Interrupt is generated after 2 samples
-    AD1CHSbits.CH0SA = 4; // replace x by the desired input
+    AD1CON2bits.SMPI = 2-1; // Interrupt is generated after N samples
+    AD1CHSbits.CH0SA = 4; // replace x by the desired input analog channel (0 to 15)
     AD1CON1bits.ON = 1; // Enable A/D converter
-
-    //Config Timer 2
+    
+    // Config Timer 2
     T2CONbits.TCKPS = 2; // 1:4 prescaler 
     PR2 = 41666; // Fout = 20MHz / (4 * (41666 + 1)) = 120 Hz
     TMR2 = 0; // Clear timer T2 count register
@@ -28,36 +28,34 @@ int main(void){
     IEC0bits.T2IE = 1; // Enable timer T2 interrupts
     IFS0bits.T2IF = 0; // Reset timer T2 interrupt flag
 
-    EnableInterrupts();
-
-    while (1){
-        // wait 100ms 
-        delay(100);
+    int i;
+    int value;
+    while(1){
+        delay(100); // 10 Hz => 100 ms
         AD1CON1bits.ASAM = 1; // Start conversion
-		while (IFS1bits.AD1IF == 0); // Wait while conversion not done (AD1IF == 0)
-        int sum = 0;
+        while( IFS1bits.AD1IF == 0 ); // Wait while conversion not done
+
         int *p = (int *)(&ADC1BUF0);
-        int i;
-        for(i = 0; i < 2, i++){
-            sum += voltageConversion(p[i*4])
+        for(i = 0; i < 2; i++){
+            value += p[i*4];
         }
-        voltage = sum/2;
+        value = value / 2;
+        voltage = voltageConversion(value);
         IFS1bits.AD1IF = 0;
-		IFS0bits.T3IF = 0;
+		IFS0bits.T2IF = 0;
     }
 }
 
-void _int_(8) isr_adc(void) {
-    send2displays(toBCD(voltage));
-    IFS0bits.T2IF = 0;
+unsigned char toBcd(unsigned char value){
+    return ((value / 10) << 4) + (value % 10); 
 }
 
 void send2displays(unsigned char value){
     int display7codes[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71 };
     static int displayFlag = 0;
-    unsigned char dh = value >> 4;
     unsigned char dl = value & 0x0F;
-    
+    unsigned char dh = value >> 4;
+
     if (displayFlag == 0){
         LATDbits.LATD5 = 1;
         LATDbits.LATD6 = 0;
@@ -71,23 +69,22 @@ void send2displays(unsigned char value){
     displayFlag = !displayFlag;
 }
 
-unsigned char toBCD( unsigned char value){
-    return ((value/10)<<4)+(value%10);  
-}
-
-// (0 a 3.3V) -> (0 a 45º) -> (20 a 65º)
-
-
-int voltageConversion(int voltage){
-    int temp = ((voltage * 33 + 511) / 1023);
-    int aux = (temp * (45/3.3)) + 20;
-    return aux;
-
-}
-
 void delay(int ms){
-    for(;ms>0;ms--){
+    for(;ms > 0; ms--){
         resetCoreTimer();
         while(readCoreTimer()<20000);
     }
 }
+
+int voltageConversion(int value){
+    int temp = (((3.3 * value) + 511)/1023);
+    // 0 - 3.3 | 0 - 45 => 45/3.3
+    temp = (temp*(45/3.3))+20;
+    return temp;
+}
+
+void _int_(8) isr_adc(void) {
+    send2displays(toBcd(voltage));
+    IFS0bits.T2IF = 0;
+}
+
